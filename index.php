5f42f10c84b890d8fd567b59f68a0234be8ff214
seyeonPark <sy.park@weballin.com>
2022-09-07 10:31:16 +0900

    Update index php

---
+++ index.php
@@ -0,0 +1,31 @@
+<div style="padding-top:30vh; text-align:center; font-size:25px">
+	HELLO SY<br>
+	<br>
+</div>
+
+
+<body>
+<?php
+	$a = 1;
+	var_dump($a);
+	echo "<br>";
+	
+	$b = 1.1;
+	var_dump($b);
+	echo"<br>";
+	
+	$c = "weball";
+	var_dump($c);
+	echo"<br>";
+	
+	$d = "자바스크립트";
+	var_dump($d);
+	
+	echo"<br>";
+	
+	$e = array($a, $b);
+	var_dump($e);
+	
+	
+?>
+</body>
